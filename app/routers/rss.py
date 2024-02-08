bcc804c6c5935d042e8d068200042879961f6b03
Horgeon <me@horgeon.fr>
2024-02-08 11:09:07 +0000

    Folded feed routes that fatch full content into a query parameter for existing routes

--- app/routers/rss.py
+++ app/routers/rss.py
@@ -12,40 +12,21 @@ router = APIRouter(
 
 
 @router.get('/unread', response_class=RSSResponse)
-async def rss_unread(request: Request, token: Annotated[str, Depends(get_current_token)], reader: bool = False):
+async def rss_unread(request: Request, token: Annotated[str, Depends(get_current_token)], reader: bool = False, full_content: bool = False):
     return RSSResponse((await retrieve_feed(
         request_url=request.url,
         request_token=token,
-        use_reader_url=reader
+        use_reader_url=reader,
+        get_full_content=full_content
     )))
 
 
 @router.get('/unread/{tag}', response_class=RSSResponse)
-async def rss_unread(tag: str, request: Request, token: Annotated[str, Depends(get_current_token)], reader: bool = False):
+async def rss_unread(tag: str, request: Request, token: Annotated[str, Depends(get_current_token)], reader: bool = False, full_content: bool = False):
     return RSSResponse((await retrieve_feed(
         request_url=request.url,
         request_token=token,
         tag=tag,
-        use_reader_url=reader
-    )))
-
-
-@router.get('/full/unread', response_class=RSSResponse)
-async def rss_unread(request: Request, token: Annotated[str, Depends(get_current_token)], reader: bool = False):
-    return RSSResponse((await retrieve_feed(
-        request_url=request.url,
-        request_token=token,
-        get_full_content=True,
-        use_reader_url=reader
-    )))
-
-
-@router.get('/full/unread/{tag}', response_class=RSSResponse)
-async def rss_unread(tag: str, request: Request, token: Annotated[str, Depends(get_current_token)], reader: bool = False):
-    return RSSResponse((await retrieve_feed(
-        request_url=request.url,
-        request_token=token,
-        tag=tag,
-        get_full_content=True,
-        use_reader_url=reader
+        use_reader_url=reader,
+        get_full_content=full_content
     )))
